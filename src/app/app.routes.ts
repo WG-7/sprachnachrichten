48a1e6664cc6d9f7763d064be8fc010973f006b7
WG-7 <155075911+WG-7@users.noreply.github.com>
2025-01-19 12:23:03 +0100

    routing fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'qr', component: QrCodeComponent },
+    {
+        path: '', component: HomeComponent, children: [
+            { path: 'qr', component: QrCodeComponent },
+        ]
+    },
     { path: '**', component: PageNotFoundComponent },
 ];
